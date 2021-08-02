44456bb8745d792deb89b52530157a757dc427bc
Andreas Evers <aevers@gmx.de>
2021-08-02 15:20:22 +0200

    Enable deg2dec function for southern and western hemisphere
    deg2dec helper function can now properly handle latitudes in the
    southern hemisphere and longitudes in the eastern hemisphere. It
    expects negative degree values for those. Also a bunch of testcases
    have been added which can be executed by calling coord.py script
    directly.
    Yet a test with real life data should be done as it is unclear how
    eastern and southern hemisphere data is represented in Vantrue MP4
    files.

--- coord.py
+++ coord.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import math
 
 def truncate(f, n):
     '''Truncates/pads a float f to n decimal places without rounding'''
@@ -9,20 +10,29 @@ def truncate(f, n):
     return '.'.join([i, (d+'0'*n)[:n]])
 
 def deg2dec (degr, min, sec):
-    val = (degr + (min / 60) + (sec / 3600))
+    val = (abs(degr) + (min / 60) + (sec / 3600)) * math.copysign(1, degr)
     val = truncate(val, 5)
-    return val
+    return float(val)
 
 def main ():
-    lng_deg = 6
-    lng_min = 4
-    lng_sec = 5426
-    lat_deg = 51
-    lat_min = 51
-    lat_sec = 750
+    testcases = [
+        ((52, 51, 7.50), 52.85208, (6, 4, 54.26), 6.08174),
+        ((-52, 51, 7.50), -52.85208, (-6, 4, 54.26), -6.08174),
+        ((0, 0, 0), 0, (0, 0, 0), 0),
+        ((-75, 15, 40.55), -75.26126, (156, 58, 28.26), 156.97451),
+        ((-75, 15, 40.55), -75.26126, (-156, 58, 28.26), -156.97451),
+        ((90, 0, 0), 90, (180, 0, 0), 180),
+        ((-90, 0, 0), -90, (-180, 0, 0), -180)
+    ]
 
-    print ("{} {} {} = {}".format(lat_deg, lat_min, lat_sec/100, deg2dec(lat_deg, lat_min, lat_sec/100)))
-    print ("{} {} {} = {}".format(lng_deg, lng_min, lng_sec/100, deg2dec(lng_deg, lng_min, lng_sec/100)))
+    for testcase in testcases:
+        print ("Test case: " + str(testcase))
+        latitude = deg2dec(testcase[0][0], testcase[0][1], testcase[0][2])
+        print("Expected latitude {} calculated latitude {}".format(testcase[1], latitude))
+        assert(abs(latitude - testcase[1]) <= 0.00001)
+        longitude = deg2dec(testcase[2][0], testcase[2][1], testcase[2][2])
+        print("Expected latitude {} calculated latitude {}".format(testcase[3], longitude))
+        assert(abs(longitude - testcase[3]) <= 0.00001)
 
 if __name__ == "__main__":
     main()
--- todo.md
+++ todo.md
@@ -1,9 +1,9 @@
 # Todo-List
 
-* Check coordinate calcuation for various values in all four hemispheres
 * Clean-up code
 * Proper Error-handling
 * Basic documentation
 * MIT License
 * Publish on github / gitlab
 * Running in container
+* Get real life data from southern and eastern hemisphere and check if transformation into GPS works properly
